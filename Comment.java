9c6b946e90009722287d04e051dd4f943479c338
dzuhrillah14 <108688694+dzuhrillah14@users.noreply.github.com>
2025-12-27 23:03:17 +0700

    sourcecode

---
+++ Comment.java
@@ -0,0 +1,19 @@
+public class Comment {
+    private String author;
+    private String text;
+    private int rating;
+
+    public Comment(String author, String text, int rating) {
+        this.author = author;
+        this.text = text;
+        this.rating = rating;
+    }
+
+    public String getAuthor() { return author; }
+    public String getText() { return text; }
+    public int getRating() { return rating; }
+
+    public String toString() {
+        return "Author: " + author + " | Rating: " + rating + " | Comment: " + text;
+    }
+}
